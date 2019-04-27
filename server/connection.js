483e2dd102cec14b1c3fc7f249f6ada9e83ebd24
Scott Wade <sc0ttwad3@gmail.com>
2019-04-27 13:52:33 -0400

    Update to correct elasticsearch node module and connection code

--- server/connection.js
+++ server/connection.js
@@ -1,10 +1,9 @@
-const elasticsearch = require('elasticsearch');
+const {Client} = require('@elastic/elasticsearch');
 
 const index = 'library';
 const type = 'novel';
-const port = 9200;
-const host = process.env.ES_HOST || 'localhost';
-const client = new elasticsearch.Client({host, port});
+
+const client = new Client({node: 'http://localhost:9200'});
 
 /** Check the ES connection status */
 async function checkConnection() {
