ae4aa9b265db4a2c6da85b6c192fbb6d16fb0138
Scott Wade <sc0ttwad3@gmail.com>
2019-04-27 13:22:51 -0400

    Add server checkConnection code

---
+++ server/connection.js
@@ -0,0 +1,24 @@
+const elasticsearch = require('elasticsearch');
+
+const index = 'library';
+const type = 'novel';
+const port = 9200;
+const host = process.env.ES_HOST || 'localhost';
+const client = new elasticsearch.Client({host, port});
+
+/** Check the ES connection status */
+async function checkConnection() {
+  let isConnected = false;
+  while (!isConnected) {
+    console.log('Connecting to ES');
+    try {
+      const health = await client.cluster.health({});
+      console.log(health);
+      isConnected = true;
+    } catch (err) {
+      console.log('Connection failed, retrying...', err);
+    }
+  }
+}
+
+checkConnection();
